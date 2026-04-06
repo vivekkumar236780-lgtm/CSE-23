44b6670ffae483a9bc0fec6b67fff5a38a886c9b
vivek kumar <vivekkumar236780@gmail.com>
2026-04-06 16:27:43 +0530

    initial commit

--- JS/script.js
+++ JS/script.js
@@ -16,3 +16,23 @@ console.log(typeof(c));
 const n="name"; //constant variable
 console.log(n);
 // n="new name"; //error because we cannot change the value of constant variable
+
+//arithmetic operators
+let x=5;
+let y=2;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y); //modulus operator gives remainder value
+console.log(x**y); //exponentiation operator
+
+//logical operators
+let p=6;
+let q=3;
+let cond1=p>q; //true
+let cond2 = p==6; //true
+console.log(cond1 && cond2); //logical and operator give true if both condition are true
+console.log(p<q || p==6); //logical or operator give true if any one condition is true
+console.log(!cond1); //logical not operator does inverse of condition
+ 
